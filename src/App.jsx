ec6698a195ff0e6d5c49730638d0e99d07814c6b
Jinsorei <eeijnl@gmail.com>
2024-10-21 21:48:07 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import Input from './components/Input';
+
+function App() {
+  return (
+    <>
+      <div className='px-10 m-12'>
+        <h1 className='text-4xl text-pink-500 my-8'>Dự án Script generator</h1>
+        <p className='my-8'>
+          Cách sử dụng: Lorem ipsum dolor sit amet consectetur, adipisicing
+          elit. Dignissimos quas ipsa asperiores minima, obcaecati voluptatem
+          maiores reprehenderit quis ut. Voluptatum culpa enim delectus
+          assumenda, qui similique modi ipsam voluptas ex.
+        </p>
+        <Input></Input>
+      </div>
+    </>
+  );
+}
+
+export default App;
