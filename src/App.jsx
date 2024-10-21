b4da4345d70c1c34155007ed1b2eec675b916806
Hainammm309 <haiinamm04@gmail.com>
2024-10-21 22:14:41 +0700

    thay đổi màu thẻ h1

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className='px-10 m-12'>
-        <h1 className='text-4xl text-pink-500 my-8'>Dự án Script generator</h1>
+        <h1 className='text-4xl text-blue-500 my-8'>Dự án Script generator</h1>
         <p className='my-8'>
           Cách sử dụng: Lorem ipsum dolor sit amet consectetur, adipisicing
           elit. Dignissimos quas ipsa asperiores minima, obcaecati voluptatem
